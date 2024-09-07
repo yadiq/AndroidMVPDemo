f90905355b849cca61123a7a18b62045aa167504
gyd <guoyadi@live.cn>
2024-09-07 16:45:55 +0800

    获取ip地址

--- app/src/main/java/com/hqumath/demo/utils/DeviceUtil.java
+++ app/src/main/java/com/hqumath/demo/utils/DeviceUtil.java
@@ -2,6 +2,8 @@ package com.hqumath.demo.utils;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Build;
@@ -9,7 +11,10 @@ import android.provider.Settings;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 
+import java.net.Inet4Address;
+import java.net.InetAddress;
 import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.util.Enumeration;
 
 import static android.content.Context.TELEPHONY_SERVICE;
@@ -86,6 +91,56 @@ public class DeviceUtil {
         if (!TextUtils.isEmpty(eth0MAC)) return eth0MAC;
         if (!TextUtils.isEmpty(wlan0MAC)) return wlan0MAC;
         return null;
+    }
+
+    /**
+     * 获得IP地址，分为三种情况，一是wifi下，二是移动网络下，三是有线网络, 得到的ip地址是不一样的
+     */
+    public static String getIPAddress() {
+        Context context = CommonUtil.getContext();
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = cm.getActiveNetworkInfo();
+        if (info != null && info.isConnected()) {
+            if (info.getType() == ConnectivityManager.TYPE_WIFI) {
+                //当前使用无线网络
+                WifiManager wifiManager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+                WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+                //调用方法将int转换为地址字符串
+                String ipAddress = intIP2StringIP(wifiInfo.getIpAddress());//得到IPV4地址
+                return ipAddress;
+            } else {//当前使用2G/3G/4G网络 使用有线网络
+                try {
+                    //Enumeration<NetworkInterface> en=NetworkInterface.getNetworkInterfaces();
+                    for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements(); ) {
+                        NetworkInterface intf = en.nextElement();
+                        for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements(); ) {
+                            InetAddress inetAddress = enumIpAddr.nextElement();
+                            if (!inetAddress.isLoopbackAddress() && inetAddress instanceof Inet4Address) {
+                                return inetAddress.getHostAddress();
+                            }
+                        }
+                    }
+                } catch (SocketException e) {
+                    e.printStackTrace();
+                }
+            }
+        } else {
+            CommonUtil.toast("当前无网络连接,请在设置中打开网络");
+        }
+        return null;
+    }
+
+    /**
+     * 将得到的int类型的IP转换为String类型
+     *
+     * @param ip
+     * @return
+     */
+    public static String intIP2StringIP(int ip) {
+        return (ip & 0xFF) + "." +
+                ((ip >> 8) & 0xFF) + "." +
+                ((ip >> 16) & 0xFF) + "." +
+                (ip >> 24 & 0xFF);
     }
 
     /**
